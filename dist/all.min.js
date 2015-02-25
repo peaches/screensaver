b4a5bf13d4ad748707f166a1d3060d8768f35a5d
Anh-Kiet Ngo <anhkiet@gmail.com>
2015-02-25 10:18:59 -0800

    Initial commit

---
+++ dist/all.min.js
@@ -0,0 +1,6 @@
+var $ = window.$;
+
+$( function()  {
+  //$('main img').attr('src', 'https://lh3.googleusercontent.com/-7L5ck8evgcE/VOwlB0ebIVI/AAAAAAAAAoc/vsfPo4X5Bek/s1600/DSC_0115.JPG');
+  $('main img').attr('src', 'https://lh3.googleusercontent.com/-FjRXfJ5yWGk/VOwlBgvw50I/AAAAAAAAAoc/S15mCt7EQUs/s1600/faces.jpg');
+});
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+
+var es6transpiler = require('gulp-es6-transpiler');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    host: '0.0.0.0',
+    port: '1234',
+    root: 'dist',
+    livereload: true
+  });
+});
+
+// Lint Task
+gulp.task('lint', function() {
+  return gulp.src('scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+  return gulp.src('scripts/*.js')
+    .pipe(es6transpiler())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('all.min.js'))
+    //.pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch('scripts/*.js', ['lint', 'scripts']);
+  gulp.watch('index.html', ['html']);
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'html', 'scripts', 'connect', 'watch']);
